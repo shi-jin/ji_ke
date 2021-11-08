f8b8f58f15f5e3656a8f706043a30021585d6f5f
shi-jin <68468615+shi-jin@users.noreply.github.com>
2021-11-08 14:49:08 +0800

    week9

---
+++ jsx1/dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./main.js":
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module 'babel-core'\\nRequire stack:\\n- E:\\\\github1.0\\\\week8\\\\jsx\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js\\n- C:\\\\Users\\\\ASUS\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\webpack\\\\node_modules\\\\loader-runner\\\\lib\\\\loadLoader.js\\n- C:\\\\Users\\\\ASUS\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\webpack\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js\\n- C:\\\\Users\\\\ASUS\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\webpack\\\\lib\\\\NormalModuleFactory.js\\n- C:\\\\Users\\\\ASUS\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\webpack\\\\lib\\\\Compiler.js\\n- C:\\\\Users\\\\ASUS\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\webpack\\\\lib\\\\webpack.js\\n- C:\\\\Users\\\\ASUS\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\webpack\\\\lib\\\\index.js\\n- C:\\\\Users\\\\ASUS\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\webpack-cli\\\\lib\\\\webpack-cli.js\\n- C:\\\\Users\\\\ASUS\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\webpack-cli\\\\lib\\\\bootstrap.js\\n- C:\\\\Users\\\\ASUS\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\webpack-cli\\\\bin\\\\cli.js\\n- C:\\\\Users\\\\ASUS\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\webpack\\\\bin\\\\webpack.js\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:902:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:746:27)\\n    at Module.require (internal/modules/cjs/loader.js:974:19)\\n    at require (internal/modules/cjs/helpers.js:93:18)\\n    at Object.<anonymous> (E:\\\\github1.0\\\\week8\\\\jsx\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js:3:13)\\n    at Module._compile (internal/modules/cjs/loader.js:1085:14)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1114:10)\\n    at Module.load (internal/modules/cjs/loader.js:950:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:790:12)\\n    at Module.require (internal/modules/cjs/loader.js:974:19)\");\n\n//# sourceURL=webpack://jsx/./main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./main.js"]();
+/******/ 	
+/******/ })()
+;
